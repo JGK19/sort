57c97222cb8311365d96223ce8f5d1db8a2e5e0e
JGK19 <106597685+JGK19@users.noreply.github.com>
2023-08-21 01:55:20 -0300

    headers

---
+++ sort.h
@@ -0,0 +1,12 @@
+
+void troca(int *a, int *b, long int *t);
+
+void selection(int *array, int q, long int *t, long int *cp);
+
+void bubble(int *array, int q, long int *t, long int *cp);
+
+void insertion(int *array, int q, long int *t, long int *mov, long int *cp);
+
+void printArray(int *array, int q);
+
+void createArray(int *array, int q);
