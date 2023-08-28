c63293e59f75cbc4ccff01274dda26f40fbc379c
JGK19 <106597685+JGK19@users.noreply.github.com>
2023-08-27 23:27:53 -0300

    merge sort

--- sort.c
+++ sort.c
@@ -53,3 +53,51 @@ void printArray(int *array, int q) {
   printf("]");
   printf("\n");
 }
+
+void mergeSort(int *array, int i, int f) {
+  int meio = (i+f)/2;
+  if (i != f) {
+    mergeSort(array, i, meio);
+    mergeSort(array, meio+1, f);
+
+    merge(array, i, meio, f);
+  }
+}
+
+void merge(int *array, int i, int meio,int f) {
+  int x, y, j, choice, qt;
+  qt = (f + 1) - i;
+  int a[qt];
+  x = i;
+  y = meio + 1;
+  
+  for (int k = 0; k < qt; k++) {
+    
+    choice = cmp(array, &x, &y, i, meio, f);
+    a[k] = choice;
+  }
+  j = 0;
+  for (int k = i; k <= f; k++) {
+    array[k] = a[j];
+    j++;
+  }
+}
+
+int cmp(int *array, int *x, int *y, int i, int meio, int f) {
+  int result, compare;
+  if (*x == *y) {
+    result = array[*x];
+    *x = *x + 1;
+    *y = *x;
+    return result;
+}
+  
+  if (array[*x] < array[*y]) {
+    result = array[*x];
+    *x = (*x < meio) ? (*x + 1): (*y);
+  } else {
+    result = array[*y];
+    *y = (*y < f) ? (*y + 1): (*x);
+  }
+  return result;
+}
